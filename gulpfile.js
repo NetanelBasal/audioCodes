436d270fe44d347d0929fe86d22c30a48f0348b6
netanelbasal <netanel7799@gmail.com>
2015-04-26 10:46:30 +0300

    build gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+gulp.task('less', function() {
+  return gulp.src('./less/main.less')
+    .pipe($.less())
+    .pipe($.autoprefixer({
+      browsers: ['last 5 versions'],
+      cascade: false
+    }))
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('./css'))
+    .pipe(reload({stream:true}));
+});
+
+
+
+gulp.task('serve', ['less'], function() {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("less/**/*.less", ['less']);
+  gulp.watch("./*.html").on('change', reload);
+});
+
+
+
+gulp.task('default', ['serve']);
